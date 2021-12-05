c34b1d641caed3bb289c45daa1adc07bb86cd469
bunyamin <bunyaminbektas2844@gmail.com>
2021-12-05 17:00:35 +0000

    first commit

---
+++ src/my_git_repoo/firstClass.java
@@ -0,0 +1,11 @@
+package my_git_repoo;
+
+public class firstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Hello World");
+	}
+
+}
